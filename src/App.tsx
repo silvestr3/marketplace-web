497ccae51c67a6e0f92f7d292c40c6c6c2c930dd
silvestr3 <fellipe.silvestre@outlook.com>
2024-11-06 19:09:31 -0300

    chore: :wrench: Configure global colors and typography styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export function App() {
-  return <></>;
+  return (
+    <div className="bg-gray-700 flex items-center justify-center flex-col text-shape-white h-screen">
+      <h1 className="title-lg">Large</h1>
+      <h2 className="title-md">Medium</h2>
+      <h3 className="title-sm">Small</h3>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,35 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		fontFamily: {
+			poppins: ['Poppins', 'sans-serif'],
+			dmsans: ['DM Sans', 'sans-serif']
+		},
   		colors: {
+			orange: {
+				base: '#F24D0D',
+				dark: '#C43C08',
+			},
+			blue: {
+				light: '#D7EFF9',
+				base: '#5EC5FD',
+				dark: '#009CF0'
+			},
+			shape: {
+				white: '#FFFFFF',
+				background: '#FBF4F4',
+				shape: '#F5EAEA'
+			},
+			gray: {
+				100: '#ADADAD',
+				200: '#949494',
+				300: '#666666',
+				400: '#3D3D3D',
+				500: '#1D1D1D',
+			},
+			danger: '#DC3545',
+			success: '#28A745',
+
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
